80b689364b6a82c57da90bea143de73386a630fc
JJBR13 <jjbrown1308@gmail.com>
2023-01-30 11:38:47 +0000

    Migrated database & Set-up super user

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo.views import get_todo_list
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_todo_list, name='get_todo_list')
 ]
