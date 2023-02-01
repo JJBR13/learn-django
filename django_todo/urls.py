3789e13760d3e3154269d305ac5fbf243adc6bbb
JJBR13 <jjbrown1308@gmail.com>
2023-02-01 10:05:14 +0000

    Add functionality to edit and POST item

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list, add_item
+from todo.views import get_todo_list, add_item, edit_item
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', get_todo_list, name='get_todo_list'),
-    path('add', add_item, name='add')
+    path('add', add_item, name='add'),
+    path('edit/<int:item_id>', edit_item, name='edit'),
 ]
