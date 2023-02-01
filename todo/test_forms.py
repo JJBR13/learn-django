247ba9eeda8d1dc3f87df581ffab081154de4431
JJBR13 <jjbrown1308@gmail.com>
2023-02-01 11:02:28 +0000

    Add test for form to test_forms.py

---
+++ todo/test_forms.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from .forms import ItemForm
+
+
+class TestItemForm(TestCase):
+
+    def test_item_name_is_required(self):
+        form = ItemForm({'name': ''})
+        # checking form is not valid
+        self.assertFalse(form.is_valid())
+        # error occured on name field
+        self.assertIn('name', form.errors.keys())
+        # error message is what we expect
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_done_field_is_not_required(self):
+        form = ItemForm({'name': 'Test Todo Item'})
+        self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = ItemForm()
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
