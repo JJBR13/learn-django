2ac3c233a8f7aafaf905f5b9fa3b25792f42e6d4
JJBR13 <jjbrown1308@gmail.com>
2023-02-02 09:44:44 +0000

    Link changes

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -15,6 +15,9 @@ import os
 import dj_database_url
 import env
 
+if os.path.isfile("env.py"):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
